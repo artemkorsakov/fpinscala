f7c81f9389510c79f6bfc40082c72b73051f7186
Артём <artemkorsakov@mail.ru>
2022-08-03 21:55:48 +0300

    Exercises 7

--- src/main/scala/fpinscala/exercises/parallelism/Nonblocking.scala
+++ src/main/scala/fpinscala/exercises/parallelism/Nonblocking.scala
@@ -1,7 +1,7 @@
 package fpinscala.exercises.parallelism
 
-import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
 import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
 
 object Nonblocking:
 
@@ -21,46 +21,46 @@ object Nonblocking:
     def fork[A](a: => Par[A]): Par[A] =
       es => cb => eval(es)(a(es)(cb))
 
-    /**
-     * Helper function for constructing `Par` values out of calls to non-blocking continuation-passing-style APIs.
-     * This will come in handy in Chapter 13.
-     */
-    def async[A](f: (A => Unit) => Unit): Par[A] = 
+    /** Helper function for constructing `Par` values out of calls to non-blocking continuation-passing-style APIs. This
+      * will come in handy in Chapter 13.
+      */
+    def async[A](f: (A => Unit) => Unit): Par[A] =
       es => cb => f(cb)
 
-    /**
-     * Helper function, for evaluating an action
-     * asynchronously, using the given `ExecutorService`.
-     */
+    /** Helper function, for evaluating an action asynchronously, using the given `ExecutorService`.
+      */
     def eval(es: ExecutorService)(r: => Unit): Unit =
-      es.submit(new Callable[Unit] { def call = r })
+      es.submit(new Callable[Unit] { def call: Unit = r })
 
     extension [A](p: Par[A])
       def run(es: ExecutorService): A =
         val ref = new AtomicReference[A] // A mutable, threadsafe reference, to use for storing the result
         val latch = new CountDownLatch(1) // A latch which, when decremented, implies that `ref` has the result
-        p(es) { a => ref.set(a); latch.countDown } // Asynchronously set the result, and decrement the latch
-        latch.await // Block until the `latch.countDown` is invoked asynchronously
+        p(es) { a =>
+          ref.set(a); latch.countDown()
+        } // Asynchronously set the result, and decrement the latch
+        latch.await() // Block until the `latch.countDown` is invoked asynchronously
         ref.get // Once we've passed the latch, we know `ref` has been set, and return its value
 
       def map2[B, C](p2: Par[B])(f: (A, B) => C): Par[C] =
-        es => cb => {
-          var ar: Option[A] = None
-          var br: Option[B] = None
-          // this implementation is a little too liberal in forking of threads -
-          // it forks a new logical thread for the actor and for stack-safety,
-          // forks evaluation of the callback `cb`
-          val combiner = Actor[Either[A,B]](es) {
-            case Left(a) =>
-              if br.isDefined then eval(es)(cb(f(a, br.get)))
-              else ar = Some(a)
-            case Right(b) =>
-              if ar.isDefined then eval(es)(cb(f(ar.get, b)))
-              else br = Some(b)
+        es =>
+          cb => {
+            var ar: Option[A] = None
+            var br: Option[B] = None
+            // this implementation is a little too liberal in forking of threads -
+            // it forks a new logical thread for the actor and for stack-safety,
+            // forks evaluation of the callback `cb`
+            val combiner = Actor[Either[A, B]](es) {
+              case Left(a) =>
+                if br.isDefined then eval(es)(cb(f(a, br.get)))
+                else ar = Some(a)
+              case Right(b) =>
+                if ar.isDefined then eval(es)(cb(f(ar.get, b)))
+                else br = Some(b)
+            }
+            p(es)(a => combiner ! Left(a))
+            p2(es)(b => combiner ! Right(b))
           }
-          p(es)(a => combiner ! Left(a))
-          p2(es)(b => combiner ! Right(b))
-        }
 
       def map[B](f: A => B): Par[B] =
         es => cb => p(es)(a => eval(es)(cb(f(a))))
@@ -68,7 +68,7 @@ object Nonblocking:
       def flatMap[B](f: A => Par[B]): Par[B] =
         es => cb => p(es)(a => f(a)(es)(cb))
 
-      def zip[B](b: Par[B]): Par[(A,B)] = map2(b)((_,_))
+      def zip[B](b: Par[B]): Par[(A, B)] = map2(b)((_, _))
 
     def lazyUnit[A](a: => A): Par[A] =
       fork(unit(a))
@@ -78,7 +78,7 @@ object Nonblocking:
 
     def sequenceRight[A](as: List[Par[A]]): Par[List[A]] =
       as match
-        case Nil => unit(Nil)
+        case Nil    => unit(Nil)
         case h :: t => h.map2(fork(sequence(t)))(_ :: _)
 
     def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
@@ -115,27 +115,31 @@ object Nonblocking:
      * about `t(es)`? What about `t(es)(cb)`?
      */
     def choice[A](p: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
-      es => cb => p(es) { b =>
-        if b then eval(es)(t(es)(cb))
-        else eval(es)(f(es)(cb))
-      }
+      es =>
+        cb =>
+          p(es) { b =>
+            if b then eval(es)(t(es)(cb))
+            else eval(es)(f(es)(cb))
+          }
 
     /* The code here is very similar. */
-    def choiceN[A](p: Par[Int])(ps: List[Par[A]]): Par[A] =
-      ???
+    def choiceN[A](p: Par[Int])(ps: List[Par[A]]): Par[A] = {
+      val l = ps.length
+      es => cb => p(es)(ind => eval(es)(ps(ind % l)(es)(cb)))
+    }
 
-    def choiceViaChoiceN[A](a: Par[Boolean])(ifTrue: Par[A], ifFalse: Par[A]): Par[A] =
-      ???
+    def choiceViaChoiceN[A](cond: Par[Boolean])(ifTrue: Par[A], ifFalse: Par[A]): Par[A] =
+      choiceN(cond.map(b => if b then 0 else 1))(List(ifTrue, ifFalse))
 
     def choiceMap[K, V](p: Par[K])(ps: Map[K, Par[V]]): Par[V] =
-      ???
+      es => cb => p(es) { k => eval(es)(ps(k)(es)(cb)) }
 
     /* `chooser` is usually called `flatMap` or `bind`. */
     def chooser[A, B](p: Par[A])(f: A => Par[B]): Par[B] =
-      ???
+      es => cb => p(es) { a => eval(es)(f(a)(es)(cb)) }
 
     def choiceViaFlatMap[A](p: Par[Boolean])(f: Par[A], t: Par[A]): Par[A] =
-      ???
+      Par.chooser(p)(b => if b then t else f)
 
     def choiceNViaFlatMap[A](p: Par[Int])(choices: List[Par[A]]): Par[A] =
       ???
@@ -146,5 +150,5 @@ object Nonblocking:
     def joinViaFlatMap[A](a: Par[Par[A]]): Par[A] =
       ???
 
-    def flatMapViaJoin[A,B](p: Par[A])(f: A => Par[B]): Par[B] =
+    def flatMapViaJoin[A, B](p: Par[A])(f: A => Par[B]): Par[B] =
       ???
