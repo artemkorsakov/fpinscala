8e07b06205f1bd44fc5b7377a293404bc0afcd9b
Артём <artemkorsakov@mail.ru>
2022-08-04 08:52:08 +0300

    Exercises 8

--- src/test/scala/fpinscala/exercises/parsing/ParsersSuite.scala
+++ src/test/scala/fpinscala/exercises/parsing/ParsersSuite.scala
@@ -9,7 +9,6 @@ import fpinscala.exercises.parsing.Parsers
 class ParsersSuite extends PropSuite:
   import UnitTestParser.*
 
-  /*
   test("Parsers.char")(genChar) { c =>
     assertEquals(char(c).run(c.toString), Right(c))
     val anotherChar = (c + 1).toChar
@@ -24,6 +23,7 @@ class ParsersSuite extends PropSuite:
     assert(string(nonEmptyS).run(anotherString).isLeft)
   }
 
+  /*
   test("Parsers.or")(Gen.unit(())) { _ =>
     val orParser = string("abra").or(string("cadabra"))
     assertEquals(orParser.run("abra"), Right("abra"))
@@ -143,7 +143,7 @@ class ParsersSuite extends PropSuite:
     assert(nConsecutiveAs.run("aaaa").isLeft)
     assert(nConsecutiveAs.run("-4aaaa").isLeft)
   }
+  */
 
   private def unbiasL[A, B, C](p: ((A, B), C)): (A, B, C) = (p(0)(0), p(0)(1), p(1))
   private def unbiasR[A, B, C](p: (A, (B, C))): (A, B, C) = (p(0), p(1)(0), p(1)(1))
-  */
