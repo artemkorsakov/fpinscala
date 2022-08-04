dfb707ac6efeec96f9046346f087735c72ba31a9
Артём <artemkorsakov@mail.ru>
2022-08-04 09:00:53 +0300

    Exercises 9

--- src/main/scala/fpinscala/exercises/parsing/Parsers.scala
+++ src/main/scala/fpinscala/exercises/parsing/Parsers.scala
@@ -13,22 +13,23 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
 
 case class Location(input: String, offset: Int = 0):
 
-  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val line: Int = input.slice(0,offset+1).count(_ == '\n') + 1
+  lazy val col: Int = input.slice(0,offset+1).reverse.indexOf('\n')
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
 
-  def advanceBy(n: Int) = copy(offset = offset+n)
+  def advanceBy(n: Int): Location = copy(offset = offset+n)
 
   def remaining: String = ???
 
-  def slice(n: Int) = ???
+  def slice(n: Int): String = ???
 
   /* Returns the line corresponding to this location */
   def currentLine: String = 
     if (input.length > 1) input.linesIterator.drop(line-1).next()
     else ""
+
 end Location
 
 case class ParseError(stack: List[(Location,String)] = List(),
