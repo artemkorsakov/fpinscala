d52c8495621c60d75175e6a08adb74588072bd66
Артём <artemkorsakov@mail.ru>
2022-08-08 10:11:54 +0300

    Exercises 9

--- src/main/scala/fpinscala/exercises/parsing/Parsers.scala
+++ src/main/scala/fpinscala/exercises/parsing/Parsers.scala
@@ -2,9 +2,17 @@ package fpinscala.exercises.parsing
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
-  def char(c: Char): Parser[Char] = ???
+  def string(s: String): Parser[String]
 
-  extension[A] (p: Parser[A])
+  def char(c: Char): Parser[Char] =
+    string(c.toString).map(_.charAt(0))
+
+  def defaultSucceed[A](a: A): Parser[A] =
+    string("").map(_ => a)
+
+  def succeed[A](a: A): Parser[A]
+
+  extension [A](p: Parser[A])
 
     def run(input: String): Either[ParseError, A]
 
@@ -12,47 +20,68 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
 
     def |(p2: => Parser[A]): Parser[A] = p.or(p2)
 
-    def listOfN(n: Int): Parser[List[A]] = ???
+    def listOfN(n: Int): Parser[List[A]] =
+      if n <= 0 then succeed(Nil)
+      else p.map2(p.listOfN(n - 1))(_ :: _)
 
-    def map[B](f: A => B): Parser[B] = ???
+    def map[B](f: A => B): Parser[B] =
+      p.flatMap(a => succeed(f(a)))
 
-    def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] = ???
+    def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      p.product(p2).map(f.tupled)
 
-    def many: Parser[List[A]] = ???
+    def many: Parser[List[A]] =
+      p.map2(p.many)(_ :: _) | succeed(Nil)
 
-    def many1: Parser[List[A]] = ???
+    def slice: Parser[String]
 
-    def product[B](p2: => Parser[B]): Parser[(A, B)] = ???
+    def flatMap[B](f: A => Parser[B]): Parser[B]
+
+    def label(msg: String): Parser[A]
+
+    def scope(msg: String): Parser[A]
+
+    def many1: Parser[List[A]] =
+      p.map2(p.many)(_ :: _)
+
+    def product[B](p2: => Parser[B]): Parser[(A, B)] =
+      p.flatMap(a => p2.map(b => (a, b)))
 
     def **[B](p2: => Parser[B]): Parser[(A, B)] = product(p2)
 
-  object Laws {
-  }
+  object Laws:
+    import fpinscala.answers.testing.*
+
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => p1.run(s) == p2.run(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p.map(a => a))(in)
+
 }
 
 case class Location(input: String, offset: Int = 0):
 
-  lazy val line: Int = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col: Int = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val line: Int = input.slice(0, offset + 1).count(_ == '\n') + 1
+  lazy val col: Int = input.slice(0, offset + 1).reverse.indexOf('\n')
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
 
-  def advanceBy(n: Int): Location = copy(offset = offset+n)
+  def advanceBy(n: Int): Location = copy(offset = offset + n)
 
   def remaining: String = ???
 
-  def slice(n: Int): String = ???
+  def slice(n: Int): String = input.substring(offset, offset + n)
 
   /* Returns the line corresponding to this location */
-  def currentLine: String = 
-    if (input.length > 1) input.linesIterator.drop(line-1).next()
+  def currentLine: String =
+    if (input.length > 1) input.linesIterator.drop(line - 1).next()
     else ""
 
 end Location
 
-case class ParseError(stack: List[(Location,String)] = List(),
-                      otherFailures: List[ParseError] = List()):
+case class ParseError(stack: List[(Location, String)] = List(), otherFailures: List[ParseError] = List()):
   def push(loc: Location, msg: String): ParseError = ???
 
   def label(s: String): ParseError = ???
