af3b08ae133a013a73e31f58efb30d747102b4c3
Артём Корсаков <artemkorsakov@mail.ru>
2023-11-18 11:57:06 +0300

    Chapter 2

--- src/test/scala/fpinscala/exercises/gettingstarted/GettingStartedSuite.scala
+++ src/test/scala/fpinscala/exercises/gettingstarted/GettingStartedSuite.scala
@@ -2,7 +2,6 @@ package fpinscala.exercises.gettingstarted
 
 import fpinscala.answers.testing.exhaustive.*
 import fpinscala.answers.testing.exhaustive.Gen.`**`
-import fpinscala.answers.testing.exhaustive.Prop.*
 import fpinscala.exercises.common.Common.*
 import fpinscala.exercises.common.PropSuite
 import fpinscala.exercises.gettingstarted.MyProgram.factorial
