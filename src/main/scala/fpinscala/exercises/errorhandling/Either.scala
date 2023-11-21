1e485c4bda5bd65b64f19c21ec48eed90ad9c0a0
Артём Корсаков <artemkorsakov@mail.ru>
2023-11-21 09:47:13 +0300

    Chapter 4

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -28,9 +28,13 @@ enum Either[+E, +A]:
 
 object Either:
   def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
-    ???
+    es match
+      case Nil => Right(List[B]())
+      case h :: t =>
+        f(h).map2(traverse(t)(f))(_ :: _)
 
-  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = ???
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if xs.isEmpty then Left("mean of empty list!")
@@ -48,13 +52,20 @@ object Either:
       a: Either[List[E], A],
       b: Either[List[E], B],
       f: (A, B) => C
-  ): Either[List[E], C] = ???
+  ): Either[List[E], C] = (a, b) match
+    case (Right(a), Right(b)) => Right(f(a, b))
+    case (Left(e1), Left(e2)) => Left(e1 ++ e2)
+    case (Left(e), _)         => Left(e)
+    case (_, Left(e))         => Left(e)
 
   def traverseAll[E, A, B](
       as: List[A],
       f: A => Either[List[E], B]
-  ): Either[List[E], List[B]] = ???
+  ): Either[List[E], List[B]] = as match
+    case Nil => Right(Nil)
+    case h :: t =>
+      map2All(f(h), traverseAll(t, f), _ :: _)
 
   def sequenceAll[E, A](
       as: List[Either[List[E], A]]
-  ): Either[List[E], List[A]] = ???
+  ): Either[List[E], List[A]] = traverseAll(as, identity)
